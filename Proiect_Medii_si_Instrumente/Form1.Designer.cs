a61e92b07bb954cf5f18bf39a3c1530adf5a2420
Raducu <raducu.pescaru@itperspectives.ro>
2022-11-28 12:13:47 +0200

    initial commit + GUI with a few things implemented

---
+++ Proiect_Medii_si_Instrumente/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Proiect_Medii_si_Instrumente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.usernameReg = new System.Windows.Forms.TextBox();
+            this.passwordReg = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.amcontButton = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.confPassReg = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.usernameLogin = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.passwordLogin = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.inapoiButton = new System.Windows.Forms.Button();
+            this.sidebar = new System.Windows.Forms.FlowLayoutPanel();
+            this.sidebarTimer = new System.Windows.Forms.Timer(this.components);
+            this.sidebarTimer2 = new System.Windows.Forms.Timer(this.components);
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.sidebarLeft = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1089, 275);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(480, 274);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(126, 365);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // usernameReg
+            // 
+            this.usernameReg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.usernameReg.Location = new System.Drawing.Point(143, 347);
+            this.usernameReg.Name = "usernameReg";
+            this.usernameReg.Size = new System.Drawing.Size(100, 20);
+            this.usernameReg.TabIndex = 7;
+            // 
+            // passwordReg
+            // 
+            this.passwordReg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwordReg.Location = new System.Drawing.Point(143, 373);
+            this.passwordReg.Name = "passwordReg";
+            this.passwordReg.Size = new System.Drawing.Size(100, 20);
+            this.passwordReg.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox6.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(178, 280);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(173, 26);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Text = "Esti nou, cumetre?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(68, 346);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(69, 18);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "Username:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(68, 370);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(69, 18);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "Password:";
+            // 
+            // registerButton
+            // 
+            this.registerButton.Location = new System.Drawing.Point(204, 433);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(75, 23);
+            this.registerButton.TabIndex = 12;
+            this.registerButton.Text = "Register";
+            this.registerButton.UseVisualStyleBackColor = true;
+            // 
+            // amcontButton
+            // 
+            this.amcontButton.Location = new System.Drawing.Point(191, 472);
+            this.amcontButton.Name = "amcontButton";
+            this.amcontButton.Size = new System.Drawing.Size(103, 23);
+            this.amcontButton.TabIndex = 13;
+            this.amcontButton.Text = "Am deja cont";
+            this.amcontButton.UseVisualStyleBackColor = true;
+            this.amcontButton.Click += new System.EventHandler(this.amcontButton_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(712, 280);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(222, 26);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.Text = "Hai sa vezi ce am pe stoc";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(16, 396);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(121, 18);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.Text = "Confirm Password:";
+            // 
+            // confPassReg
+            // 
+            this.confPassReg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.confPassReg.Location = new System.Drawing.Point(143, 399);
+            this.confPassReg.Name = "confPassReg";
+            this.confPassReg.Size = new System.Drawing.Size(100, 20);
+            this.confPassReg.TabIndex = 16;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(657, 349);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(69, 18);
+            this.textBox5.TabIndex = 17;
+            this.textBox5.Text = "Username:";
+            // 
+            // usernameLogin
+            // 
+            this.usernameLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.usernameLogin.Location = new System.Drawing.Point(732, 350);
+            this.usernameLogin.Name = "usernameLogin";
+            this.usernameLogin.Size = new System.Drawing.Size(100, 20);
+            this.usernameLogin.TabIndex = 18;
+            // 
+            // textBox8
+            // 
+            this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox8.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox8.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(657, 373);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(69, 18);
+            this.textBox8.TabIndex = 19;
+            this.textBox8.Text = "Password:";
+            // 
+            // passwordLogin
+            // 
+            this.passwordLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwordLogin.Location = new System.Drawing.Point(732, 376);
+            this.passwordLogin.Name = "passwordLogin";
+            this.passwordLogin.Size = new System.Drawing.Size(100, 20);
+            this.passwordLogin.TabIndex = 20;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(789, 433);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 21;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // inapoiButton
+            // 
+            this.inapoiButton.Location = new System.Drawing.Point(789, 472);
+            this.inapoiButton.Name = "inapoiButton";
+            this.inapoiButton.Size = new System.Drawing.Size(75, 23);
+            this.inapoiButton.TabIndex = 22;
+            this.inapoiButton.Text = "Inapoi";
+            this.inapoiButton.UseVisualStyleBackColor = true;
+            this.inapoiButton.Click += new System.EventHandler(this.inapoiButton_Click);
+            // 
+            // sidebar
+            // 
+            this.sidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.sidebar.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.sidebar.Location = new System.Drawing.Point(546, 274);
+            this.sidebar.MaximumSize = new System.Drawing.Size(541, 359);
+            this.sidebar.Name = "sidebar";
+            this.sidebar.Size = new System.Drawing.Size(541, 359);
+            this.sidebar.TabIndex = 23;
+            // 
+            // sidebarTimer
+            // 
+            this.sidebarTimer.Interval = 20;
+            this.sidebarTimer.Tick += new System.EventHandler(this.sidebarTimer_Tick);
+            // 
+            // sidebarTimer2
+            // 
+            this.sidebarTimer2.Interval = 20;
+            this.sidebarTimer2.Tick += new System.EventHandler(this.sidebarTimer2_Tick);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(-1, 274);
+            this.flowLayoutPanel1.MaximumSize = new System.Drawing.Size(541, 359);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(0, 0);
+            this.flowLayoutPanel1.TabIndex = 24;
+            // 
+            // sidebarLeft
+            // 
+            this.sidebarLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.sidebarLeft.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.sidebarLeft.Location = new System.Drawing.Point(-1, 274);
+            this.sidebarLeft.MaximumSize = new System.Drawing.Size(541, 359);
+            this.sidebarLeft.Name = "sidebarLeft";
+            this.sidebarLeft.Size = new System.Drawing.Size(1, 359);
+            this.sidebarLeft.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(1087, 632);
+            this.Controls.Add(this.sidebarLeft);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.sidebar);
+            this.Controls.Add(this.inapoiButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordLogin);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.usernameLogin);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.confPassReg);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.amcontButton);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.passwordReg);
+            this.Controls.Add(this.usernameReg);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Dorel Auto";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox passwordReg;
+        private System.Windows.Forms.TextBox usernameReg;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.Button amcontButton;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox confPassReg;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox usernameLogin;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox passwordLogin;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button inapoiButton;
+        private System.Windows.Forms.FlowLayoutPanel sidebar;
+        private System.Windows.Forms.Timer sidebarTimer;
+        private System.Windows.Forms.Timer sidebarTimer2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel sidebarLeft;
+    }
+}
+
---
+++ Proiect_Medii_si_Instrumente/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System.Windows.Forms;
+
+namespace Proiect_Medii_si_Instrumente
+{
+    public partial class Form1 : Form
+    {
+        bool sidebarExpand = true;
+        bool sidebar2Expand = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sidebarTimer_Tick(object sender, System.EventArgs e)
+        {
+            if (sidebarExpand)
+            {
+                sidebar.Width -= 10;
+                if (sidebar.Width == sidebar.MinimumSize.Width)
+                {
+                    sidebarExpand = false;
+                    sidebarTimer.Stop();
+                    sidebarTimer2.Start();
+                }
+            }
+            else
+            {
+                sidebar.Width += 10;
+                if (sidebar.Width == sidebar.MaximumSize.Width)
+                {
+                    sidebarExpand = true;
+                    sidebarTimer.Stop();
+                    if (sidebar2Expand)
+                    {
+                        sidebarTimer2.Start();
+
+                    }
+                }
+            }
+        }
+
+        private void amcontButton_Click(object sender, System.EventArgs e)
+        {
+            sidebarTimer.Start();
+        }
+
+        private void inapoiButton_Click(object sender, System.EventArgs e)
+        {
+            sidebarTimer2.Start();
+        }
+
+        private void sidebarTimer2_Tick(object sender, System.EventArgs e)
+        {
+            if (sidebar2Expand)
+            {
+                sidebarLeft.Width -= 10;
+                if (sidebarLeft.Width == sidebarLeft.MinimumSize.Width)
+                {
+                    sidebar2Expand = false;
+                    sidebarTimer2.Stop();
+                    sidebarTimer.Start();
+                }
+            }
+            else
+            {
+                sidebarLeft.Width += 10;
+                if (sidebarLeft.Width == sidebarLeft.MaximumSize.Width)
+                {
+                    sidebar2Expand = true;
+                    sidebarTimer2.Stop();
+                }
+            }
+        }
+    }
+}
