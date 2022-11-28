92598c5eaf4e9c19895ddbb3645c4740269ca3c2
Raducu <raducu.pescaru@itperspectives.ro>
2022-11-28 19:14:03 +0200

    implemented login flow + improved registration flow

--- Proiect_Medii_si_Instrumente/Form1.cs
+++ Proiect_Medii_si_Instrumente/Form1.cs
@@ -9,27 +9,67 @@ namespace Proiect_Medii_si_Instrumente
         #region Private Fields
         private bool sidebarExpand = true;
         private bool sidebar2Expand = false;
-        private readonly string DBString = "Server=Raducu-PC2; Database=BazarAuto; Trusted_Connection=True;";
+        private readonly string connString = "Server=Raducu-PC2; Database=BazarAuto; Trusted_Connection=True;";
         #endregion
 
         #region Private Methods
         private void RegisterUser()
         {
-            string connString = DBString;
             SqlConnection conn = new SqlConnection(connString);
+            var sql = "INSERT INTO Users VALUES (@username, @password)";
+            var sql2 = "SELECT * FROM USERS WHERE username = @username";
             try
             {
-                SqlCommand command = new SqlCommand("INSERT INTO Users VALUES (@username, @password)", conn);
-                command.Parameters.AddWithValue("@username", usernameReg.Text);
-                command.Parameters.AddWithValue("@password", passwordReg.Text);
+                SqlCommand command2 = new SqlCommand(sql2, conn);
+                command2.Parameters.AddWithValue("@username", usernameReg.Text);
                 conn.Open();
-                command.ExecuteNonQuery();
-                conn.Close();
-                MessageBox.Show("Te-ai inregistrat cu succes!");
+                SqlDataReader reader = command2.ExecuteReader();
+                if (reader.Read())
+                {
+                    MessageBox.Show("Deja mai exista unu' ca tine! Alege alt username.");
+                    conn.Close();
+                }
+                else
+                {
+                    conn.Close();
+                    SqlCommand command = new SqlCommand(sql, conn);
+                    command.Parameters.AddWithValue("@username", usernameReg.Text);
+                    command.Parameters.AddWithValue("@password", passwordReg.Text);
+                    conn.Open();
+                    command.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("Te-am bagat in sistem!");
+                    usernameReg.Text = null;
+                    passwordReg.Text = null;
+                    confPassReg.Text = null;
+                }
             }
             catch (Exception)
             {
                 MessageBox.Show("Connection failed!");
+                conn.Close();
+            }
+        }
+
+        private void LoginUser()
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            var sql = "SELECT * FROM USERS WHERE username = @username AND password = @password";
+            SqlCommand command = new SqlCommand(sql, conn);
+            command.Parameters.AddWithValue("@username", usernameLogin.Text);
+            command.Parameters.AddWithValue("@password", passwordLogin.Text);
+            conn.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                MessageBox.Show("User-ul exista.");
+                conn.Close();
+                usernameLogin.Text = null;
+                passwordLogin.Text = null;
+            }
+            else
+            {
+                MessageBox.Show("N-ai cont veric'.");
                 conn.Close();
             }
         }
@@ -103,14 +143,19 @@ namespace Proiect_Medii_si_Instrumente
 
         private void registerButton_Click(object sender, EventArgs e)
         {
-            if(passwordReg.Text == confPassReg.Text)
+            if (usernameReg.Text != string.Empty && passwordReg.Text != string.Empty && passwordReg.Text == confPassReg.Text)
             {
                 RegisterUser();
             }
             else
             {
-                MessageBox.Show("Passwords doesn't match!");
+                MessageBox.Show("Mai verifica datele patron.");
             }
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            LoginUser();
         }
     }
 }
